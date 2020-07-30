1e77ccca602c17459a0524ec091c281b155ef05e
Ege Aydın <ege124@gmail.com>
2020-07-30 21:39:23 +0300

    feat: created valid_schedules function and improved time_collision function

--- web_server.py
+++ web_server.py
@@ -11,29 +11,38 @@ def check_time_collision(course_combo):
         for day in course_times:
             if day not in schedule:
                 schedule[day] = []
-            for times in schedule[day]:
-                start, stop = times
-                for i in range(start, stop + 1):
-                    if i in schedule[day]:
-                        return False
-                    schedule[day].append(i)
+            if any(i in schedule[day] for i in course_times[day]):
+                return False
+            schedule[day].extend(course_times[day])
     return True
+
+
+def get_valid_schedules(wanted_courses):
+    global courses
+    wanted_course_lists = []
+    for course in wanted_courses:
+        wanted_course_lists.append([])
+        for section in courses[course][1]:
+            section_no = section[0]
+            time = section[3]
+            wanted_course_lists[-1].append([section_no, time])
+
+    all_combinations = list(product(*wanted_course_lists))
+
+    valid_combinations = []
+    for combination in all_combinations:
+        if check_time_collision(combination):
+            valid_combinations.append(combination)
+    return valid_combinations
 
 
 with open('course_data.json', 'r') as fp:
     courses = json.load(fp)
 
 wanted_courses = ["EEE 313", "EEE 351", "MBG 110", "EEE 321"]
+get_valid_schedules(wanted_courses)
 
-wanted_course_lists = []
-for course in wanted_courses:
-    wanted_course_lists.append([])
-    for section in courses[course][1]:
-        section_no = section[0]
-        time = section[3]
-        wanted_course_lists[-1].append([section_no, time])
 
-all_products = list(product(*wanted_course_lists))
 
 
 
