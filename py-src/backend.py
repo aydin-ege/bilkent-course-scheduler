10eeb1feb2ac3baa06607ffa2c512e9ef7a154ce
Ege Aydın <ege124@gmail.com>
2020-08-04 22:50:06 +0300

    feat: added time collision check

--- py-src/backend.py
+++ py-src/backend.py
@@ -4,21 +4,23 @@ import json
 with open('course_data.json', 'r') as fp:
     courses = json.load(fp)
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/api')
+@app.route('/api', methods=['POST'])
 def api():
-    wanted_courses = ["EEE 102", "EEE 211"]
-    # todo: get wanted courses
-    wanted_course_lists = {}
-    for course in wanted_courses:
-        wanted_course_lists[course] = []
-        for section in courses[course][1]:
-            wanted_course_lists[course].append([section[0], section[1], section[3]])
 
-    return wanted_course_lists
+    print(request.form['data'])
+    return {"1":"a"}
+    if request.method == 'GET':
+        wanted_course_lists = []
+        for course in request.form['data']:
+            wanted_course_lists.append({})
+            for section in courses[course][1]:
+                wanted_course_lists[-1][section[0]] = (section[1], section[3])
+
+        return {'data': wanted_course_lists}
 
 
 if __name__ == '__main__':
--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import course_codes from './course_codes.json';
 const course_prefixes = [{ value: 'ACC', label: 'ACC' }, { value: 'ADA', label: 'ADA' }, { value: 'AMER', label: 'AMER' }, { value: 'ARCH', label: 'ARCH' }, { value: 'BF', label: 'BF' }, { value: 'BIM', label: 'BIM' }, { value: 'BTE', label: 'BTE' }, { value: 'CHEM', label: 'CHEM' }, { value: 'CI', label: 'CI' }, { value: 'CINT', label: 'CINT' }, { value: 'COMD', label: 'COMD' }, { value: 'CS', label: 'CS' }, { value: 'CTE', label: 'CTE' }, { value: 'CTIS', label: 'CTIS' }, { value: 'ECON', label: 'ECON' }, { value: 'EDEB', label: 'EDEB' }, { value: 'EEE', label: 'EEE' }, { value: 'EEPS', label: 'EEPS' }, { value: 'ELIT', label: 'ELIT' }, { value: 'ELS', label: 'ELS' }, { value: 'EMBA', label: 'EMBA' }, { value: 'ENG', label: 'ENG' }, { value: 'ETE', label: 'ETE' }, { value: 'FA', label: 'FA' }, { value: 'FRP', label: 'FRP' }, { value: 'GE', label: 'GE' }, { value: 'GRA', label: 'GRA' }, { value: 'HART', label: 'HART' }, { value: 'HCIV', label: 'HCIV' }, { value: 'HIST', label: 'HIST' }, { value: 'HUM', label: 'HUM' }, { value: 'IAED', label: 'IAED' }, { value: 'IE', label: 'IE' }, { value: 'IELTS', label: 'IELTS' }, { value: 'IR', label: 'IR' }, { value: 'LAUD', label: 'LAUD' }, { value: 'LAW', label: 'LAW' }, { value: 'LNG', label: 'LNG' }, { value: 'MAN', label: 'MAN' }, { value: 'MATH', label: 'MATH' }, { value: 'MBA', label: 'MBA' }, { value: 'MBG', label: 'MBG' }, { value: 'ME', label: 'ME' }, { value: 'MIAPP', label: 'MIAPP' }, { value: 'MSC', label: 'MSC' }, { value: 'MSN', label: 'MSN' }, { value: 'MTE', label: 'MTE' }, { value: 'MUS', label: 'MUS' }, { value: 'NSC', label: 'NSC' }, { value: 'PE', label: 'PE' }, { value: 'PHIL', label: 'PHIL' }, { value: 'PHYS', label: 'PHYS' }, { value: 'POLS', label: 'POLS' }, { value: 'PREP', label: 'PREP' }, { value: 'PSYC', label: 'PSYC' }, { value: 'SFL', label: 'SFL' }, { value: 'SOC', label: 'SOC' }, { value: 'TE', label: 'TE' }, { value: 'TEFL', label: 'TEFL' }, { value: 'THEA', label: 'THEA' }, { value: 'THM', label: 'THM' }, { value: 'THR', label: 'THR' }, { value: 'TOEFL', label: 'TOEFL' }, { value: 'TRIN', label: 'TRIN' }, { value: 'TURK', label: 'TURK' }]
 
 const animatedComponents = makeAnimated();
-let  selected_courses = []
-
+let selected_courses = []
+let recvd_data = []
 
 
 class CourseSelection extends React.Component {
@@ -70,7 +70,7 @@ class CourseSelection extends React.Component {
 
         return (
             <Select
-                
+
                 closeMenuOnSelect={false}
                 components={animatedComponents}
                 defaultValue={[]}
@@ -117,7 +117,7 @@ class Cell extends React.Component {
     }
     render() {
         return (
-            <td style={this.state.selected?{'background-color':'#737373'}:{}} onClick={() => {this.props.onClick(); this.setState({selected: !this.state.selected})}}>{this.props.value}</td>
+            <td style={this.state.selected ? { 'background-color': '#737373' } : {}} onClick={() => { this.props.onClick(); this.setState({ selected: !this.state.selected }) }}>{this.props.value}</td>
         )
     }
 }
@@ -174,12 +174,62 @@ class Schedule extends React.Component {
     }
 }
 
-function get_schedules(){
-    fetch('/api').then(res => res.json()).then(data => {
-        alert(JSON.stringify(data));
-    });
+function check_time_collision(course_combo) {
+    let schedule = {}
+    for (let i = 0; i < course_combo.length; i++) {
+        let course_times = recvd_data[i][course_combo[i]][1]
+        for (var day in course_times) {
+            if (!(day in schedule)) {
+                schedule[day] = []
+            }
+            if (course_times[day].some(i => schedule[day].includes(i)))
+                return false
+            schedule[day].push(...course_times[day])
+        }
+    }
+    return true
 }
 
+function product(args) {
+    return args.reduce(function tl(accumulator, value) {
+        var tmp = [];
+        accumulator.forEach(function (a0) {
+            value.forEach(function (a1) {
+                tmp.push(a0.concat(a1));
+            });
+        });
+        return tmp;
+    }, [[]]);
+}
+
+async function get_schedules() {
+    let all_combos = []
+    alert(JSON.stringify(selected_courses))
+    await fetch('/api', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'1':'aaa'})
+    })
+        .then(res => res.json())
+        .then(data => {
+            recvd_data = data.data
+            let section_list = []
+            data.data.forEach(course => {
+                section_list.push(Object.keys(course));
+            });
+            all_combos = product(section_list)
+        });
+
+    let valid_combos = []
+    all_combos.forEach(combo => {
+        if (check_time_collision(combo))
+            valid_combos.push(combo)
+    })
+    console.log(valid_combos)
+}
 // ========================================
 
 
