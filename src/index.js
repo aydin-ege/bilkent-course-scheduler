2c15dad053b33acea582ffca6c3e5be4c4c0eebb
Ege Aydın <ege124@gmail.com>
2020-08-06 17:37:36 +0300

    fix: Schedule is now visible

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import makeAnimated from 'react-select/animated';
 import course_codes from './course_codes.json';
 
 const course_prefixes = [{ value: 'ACC', label: 'ACC' }, { value: 'ADA', label: 'ADA' }, { value: 'AMER', label: 'AMER' }, { value: 'ARCH', label: 'ARCH' }, { value: 'BF', label: 'BF' }, { value: 'BIM', label: 'BIM' }, { value: 'BTE', label: 'BTE' }, { value: 'CHEM', label: 'CHEM' }, { value: 'CI', label: 'CI' }, { value: 'CINT', label: 'CINT' }, { value: 'COMD', label: 'COMD' }, { value: 'CS', label: 'CS' }, { value: 'CTE', label: 'CTE' }, { value: 'CTIS', label: 'CTIS' }, { value: 'ECON', label: 'ECON' }, { value: 'EDEB', label: 'EDEB' }, { value: 'EEE', label: 'EEE' }, { value: 'EEPS', label: 'EEPS' }, { value: 'ELIT', label: 'ELIT' }, { value: 'ELS', label: 'ELS' }, { value: 'EMBA', label: 'EMBA' }, { value: 'ENG', label: 'ENG' }, { value: 'ETE', label: 'ETE' }, { value: 'FA', label: 'FA' }, { value: 'FRP', label: 'FRP' }, { value: 'GE', label: 'GE' }, { value: 'GRA', label: 'GRA' }, { value: 'HART', label: 'HART' }, { value: 'HCIV', label: 'HCIV' }, { value: 'HIST', label: 'HIST' }, { value: 'HUM', label: 'HUM' }, { value: 'IAED', label: 'IAED' }, { value: 'IE', label: 'IE' }, { value: 'IELTS', label: 'IELTS' }, { value: 'IR', label: 'IR' }, { value: 'LAUD', label: 'LAUD' }, { value: 'LAW', label: 'LAW' }, { value: 'LNG', label: 'LNG' }, { value: 'MAN', label: 'MAN' }, { value: 'MATH', label: 'MATH' }, { value: 'MBA', label: 'MBA' }, { value: 'MBG', label: 'MBG' }, { value: 'ME', label: 'ME' }, { value: 'MIAPP', label: 'MIAPP' }, { value: 'MSC', label: 'MSC' }, { value: 'MSN', label: 'MSN' }, { value: 'MTE', label: 'MTE' }, { value: 'MUS', label: 'MUS' }, { value: 'NSC', label: 'NSC' }, { value: 'PE', label: 'PE' }, { value: 'PHIL', label: 'PHIL' }, { value: 'PHYS', label: 'PHYS' }, { value: 'POLS', label: 'POLS' }, { value: 'PREP', label: 'PREP' }, { value: 'PSYC', label: 'PSYC' }, { value: 'SFL', label: 'SFL' }, { value: 'SOC', label: 'SOC' }, { value: 'TE', label: 'TE' }, { value: 'TEFL', label: 'TEFL' }, { value: 'THEA', label: 'THEA' }, { value: 'THM', label: 'THM' }, { value: 'THR', label: 'THR' }, { value: 'TOEFL', label: 'TOEFL' }, { value: 'TRIN', label: 'TRIN' }, { value: 'TURK', label: 'TURK' }]
-
 const animatedComponents = makeAnimated();
 let selected_courses = []
-let recvd_data = []
-
 
 class CourseSelection extends React.Component {
     constructor(props) {
@@ -124,13 +121,15 @@ class Cell extends React.Component {
 
 
 class Schedule extends React.Component {
+
     blockCell(column, row) {
         //TODO: discard all with that cell full
     }
 
     renderCell(column, row) {
+        console.log(this.props.schedule_table)
         return (
-            <Cell value="Random stuff" pos={column * 5 + row} onClick={() => this.blockCell(column, row)}></Cell>
+            <Cell value={this.props.schedule_table[column * 5 + row]} onClick={() => this.blockCell(column, row)}></Cell>
         )
     }
 
@@ -174,63 +173,106 @@ class Schedule extends React.Component {
     }
 }
 
-function check_time_collision(course_combo) {
-    let schedule = {}
-    for (let i = 0; i < course_combo.length; i++) {
-        let course_times = recvd_data[i][course_combo[i]][1]
-        for (var day in course_times) {
-            if (!(day in schedule)) {
-                schedule[day] = []
-            }
-            if (course_times[day].some(i => schedule[day].includes(i)))
-                return false
-            schedule[day].push(...course_times[day])
-        }
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            schedule_table: [],
+            recvd_data: []
+        };
     }
-    return true
-}
 
-function product(args) {
-    return args.reduce(function tl(accumulator, value) {
-        var tmp = [];
-        accumulator.forEach(function (a0) {
-            value.forEach(function (a1) {
-                tmp.push(a0.concat(a1));
+    product(args) {
+        return args.reduce(function tl(accumulator, value) {
+            var tmp = [];
+            accumulator.forEach(function (a0) {
+                value.forEach(function (a1) {
+                    tmp.push(a0.concat(a1));
+                });
             });
-        });
-        return tmp;
-    }, [[]]);
-}
+            return tmp;
+        }, [[]]);
+    }
 
-async function get_schedules() {
-    let all_combos = []
-    let str = selected_courses.join(",")
-    console.log(JSON.stringify({"data": str}))
-    await fetch('/api', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: "data="+str
-    })
-        .then(res => res.json())
-        .then(data => {
-            recvd_data = data.data
-            let section_list = []
-            data.data.forEach(course => {
-                section_list.push(Object.keys(course));
+
+    check_time_collision(course_combo) {
+        let schedule = {}
+        for (let i = 0; i < course_combo.length; i++) {
+            let course_times = this.state.recvd_data[i][course_combo[i]][1]
+            for (var day in course_times) {
+                if (!(day in schedule)) {
+                    schedule[day] = []
+                }
+                if (course_times[day].some(i => schedule[day].includes(i)))
+                    return false
+                schedule[day].push(...course_times[day])
+            }
+        }
+        return true
+    }
+
+    create_table(valid_combo) {
+        const current_schedule = []
+        const days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 5; j++) {
+                for (let k = 0; k < valid_combo.length; k++) {
+                    let section_no = valid_combo[k]
+                    let course_name = selected_courses[k]
+                    let course_hours = this.state.recvd_data[k][section_no][1]
+                    if (course_hours[days[j]] && course_hours[days[j]].includes(i)) {
+                        current_schedule[i * 5 + j] = course_name + "-" + section_no
+                    }
+                    else if (!current_schedule[i * 5 + j])
+                        current_schedule[i * 5 + j] = ""
+                }
+            }
+        }
+        this.setState({ schedule_table: current_schedule })
+        console.log(this.state.schedule_table)
+    }
+
+
+    async get_schedules() {
+        let all_combos = []
+        let str = selected_courses.join(",")
+        await fetch('/api', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: "data=" + str
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({recvd_data: data.data})
+                let section_list = []
+                data.data.forEach(course => {
+                    section_list.push(Object.keys(course));
+                });
+                all_combos = this.product(section_list)
             });
-            all_combos = product(section_list)
-        });
 
-    let valid_combos = []
-    all_combos.forEach(combo => {
-        if (check_time_collision(combo))
-            valid_combos.push(combo)
-    })
-    console.log(valid_combos)
+        let valid_combos = []
+        all_combos.forEach(combo => {
+            if (this.check_time_collision(combo))
+                valid_combos.push(combo)
+        })
+        this.create_table(valid_combos[0])
+    }
+    render() {
+        return (
+            <div>
+                <CourseSelection />
+                <Schedule schedule_table={this.state.schedule_table} />
+                <button onClick={() => this.get_schedules()}>Get Schedules</button>
+            </div>
+        )
+    }
+
 }
-// ========================================
 
 
-ReactDOM.render(<div><CourseSelection /><Schedule /><button onClick={() => get_schedules()}>Get Schedules</button></div>, document.getElementById('root'));
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
